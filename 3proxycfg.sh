a204f0df4c43fc669338b33151770ec32be09b79
sameerlike141 <sameerlike141@gmail.com>
2023-02-08 22:16:42 +0500

    Update 3proxycfg.sh

--- 3proxycfg.sh
+++ 3proxycfg.sh
@@ -14,37 +14,38 @@ echo #allow admin
 
 # HTTP(S) прокси
 #echo "auth iponly"
-#echo "allow * 1.4.8.8"
+#echo "allow * sameer"
 
-echo "users user1:CL:user1"
+
+
+echo "Enter username:"
+read username
+
+echo "Enter password:"
+read password
+
+echo "users $username:CL:$password"
 echo "flush"
 echo "allow * * * *"
 
-port=30000
+
+echo "Enter starting port number:"
+read start_port
+
+echo "Enter the port range:"
+read port_range
+
+echo "Enter the path of the ip.list file:"
+read filepath
+
+port=$start_port
 count=1
-for i in `cat ip.list`; do
-    echo "proxy -6 -n -a -p$port -i127.0.0.1 -e$i"
+
+for i in `cat $filepath`; do
+    echo "socks -6 -n -a -p$port -i$i -e$i"
     ((port+=1))
     ((count+=1))
-    if [ $count -eq 10001 ]; then
+    if [ $count -eq $port_range ]; then
         exit
     fi
 done
-
-# Socks прокси
-# echo "#auth iponly"
-# echo "#allow * 1.4.8.8"
-# echo "users user1:CL:user1"
-# echo "flush"
-# echo "allow * * * *"
-
-# port=30000
-# count=1
-# for i in `cat ip.list`; do
-#     echo "socks -6 -p$port  -i127.0.0.1 -e$i"
-#     ((port+=1))
-#     ((count+=1))
-#     if [ $count -eq 10001 ]; then
-#         exit
-#     fi
-# done
